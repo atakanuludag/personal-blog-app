d3838a0fdc8cbb814d3eea6a4f941c27d4dd480f
Atakan Yasin Uludağ <atknuludag@gmail.com>
2024-01-29 21:35:31 +0300

    Fix: Render Markdown inTable bug
    Refactor: RenderMdx any type

--- app/src/components/RenderMdx.tsx
+++ app/src/components/RenderMdx.tsx
@@ -23,15 +23,13 @@ export default function RenderMdx({ content, theme }: RenderMdxProps) {
   };
 
   const components: MDXComponents = {
-    code({ node, inline, className, children, ...props }: any) {
-      //todo: any
+    code({ className, children, ...props }) {
       const match = /language-(\w+)/.exec(className || "");
-      return !inline && match ? (
+      return match ? (
         <SyntaxHighlighter
+          showLineNumbers
           language={match[1]}
           style={theme === PaletteMode.DARK ? materialDark : materialLight}
-          showLineNumbers
-          {...props}
         >
           {String(children).replace(/\n$/, "")}
         </SyntaxHighlighter>
